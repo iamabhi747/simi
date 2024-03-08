ad32c0e61445f34ecd3c095fd3da8b3bf589d264
Abhinandan Bhuse <i.am.abhi747@gmail.com>
2024-03-08 12:52:19 +0530

    added some constants in __init__.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,18 @@
 from .NeuralNetwork import NeuralNetwork
 from .DenseLayer import DenseLayer
 from .Perceptron import Perceptron
+from .Train import Train
+from .NonLinearFunctions import *
+from .CostFunctionFunctions import *
 
 __all__ = [
     "NeuralNetwork",
     "DenseLayer",
-    "Perceptron"
+    "Perceptron",
+    "Train",
+    "IDENTITY",
+    "RELU",
+    "SIGMOID",
+    "TANH",
+    "MSE"
 ]
