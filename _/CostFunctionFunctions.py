f7ff73c3022b7dc45646452f8dd439c0b3b69747
Abhinandan Bhuse <i.am.abhi747@gmail.com>
2024-06-10 15:01:40 +0530

    removed old code

--- _/CostFunctionFunctions.py
+++
@@ -1,7 +0,0 @@
-from .CostFunction import CostFunction
-
-MSE = CostFunction("MSE", lambda x, y: (x - y)**2, lambda x, y: 2*(x - y))
-
-COST_FUNCTIONS = {
-    "MSE": MSE
-}
--- _/NonLinear.py
+++
@@ -1,8 +0,0 @@
-
-
-class NonLinear:
-    def __init__(self, type:str, call, derv) -> None:
-        self.type = type
-        assert callable(call) and callable(derv), "call and derv must be callable"
-        self.call = call
-        self.derv = derv
